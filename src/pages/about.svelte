916bcc6a657c93b2ca3f7dad7f828c45194ea76b
benlubas <benmlubas@gmail.com>
2021-09-17 15:08:03 -0400

    typing

--- src/pages/about.svelte
+++ src/pages/about.svelte
@@ -13,7 +13,6 @@
       enjoyed the homepage, I had a ton of fun making it. I'm going to split
       this about page into catagories, so feel free to skip over the parts that
       you don't care about. First up...
-      <br />
       <div class="subtitle">Programming Experience</div>
       Woo, can you tell I'm writing this for employers? I got my start in comuper
       science in high school, where I took two into to web dev courses, APCS A and
@@ -23,12 +22,15 @@
       myself React, Node/Express, and ModgoDB and created another website for the
       school.
       <br />
+      <br />
       As a side note, my senior year I also took a Math Python course that explored
       basic mathematical modeling in Python, so I learned Python.
+      <br />
       <br />
       The next summer I learned about Svelte, and decided that I'd like to know how
       that works. I made a small practice site and followed the tutorial, and I remade
       parts of an older React project.
+      <br />
       <br />
       Then I went off to college, learned a functional language called Racket that's
       only used to teaching. After that mode of the course work has been in Java.
@@ -36,12 +38,12 @@
       courses have sprinkled in some Python here or there.
 
       <br />
+      <br />
       Of note, throughout highschool I was learning some Git, and I stored my entire
       senior year project on GitHub (in a private repo). This helped me learn terminal
       commands, and I'm learning even more of those this semester. I'm also a Vim
       user, I have the Vim extension configured on every editor that allows it.
 
-      <br />
       <div class="subtitle">This Website and Svelte</div>
       This site is built with Svelte, Svelte is basically a web framwork like React
       as far as writing components goes. The difference is in what happens next.
@@ -49,12 +51,12 @@
       JS. If you'd like to learn more, I'll link their
       <a href="https://www.svelte.dev"> site</a> and you can look around.
       <br />
-      I ran into Svelte when v 3.0 was launched, and started to learn it. I had already
+      <br />
+      I ran into Svelte when v3.0 was launched, and started to learn it. I had already
       used React, but I liked this much better. There's something about the way Svelte
       components are written, they make more sense, they're cleaner. I also like
       that it's not backed by Facebook.
 
-      <br />
       <div class="subtitle">Education&mdash;High School</div>
       I graduated from high school in 2020; I was around the top of my class. I had
       a 4.958 weighted GPA, took a bunch of AP classes, and was the assitant captain
@@ -64,10 +66,10 @@
       AP Chem. Senior year I did an independant study in web development, AP Calc
       BC, AP Stat, and AP Physics C.
       <br />
+      <br />
       You can read more about the projects that I did in my time in high school on
       the <a href={$url("/projects")}> projects page.</a>
 
-      <br />
       <div class="subtitle">Education&mdash;College</div>
       I'm currently attending Northeastern University in Boston. I'm a CS major,
       graduating in 2024 (assuming that nothing crazy happens). I'm not planning
@@ -79,11 +81,13 @@
       like this school.
 
       <br />
+      <br />
       In addition to that, I have the co-op experience coming up (this is the reason
       that I'm making this website in the first place!). I'm really excited to go
       on co-op, I'm also a little nervous for interviews, but I'll get through it.
       I'm extreamely gratefull that Northeastern has the co-op program.
 
+      <br />
       <br />
       If you want to read about my course work at Northeastern,
       <a href={$url("/courses")}> click here.</a>
--- src/pages/courses.svelte
+++ src/pages/courses.svelte
@@ -13,29 +13,70 @@
 
       <br />
       <div class="subtitle">Fundies 1 & 2</div>
-      These are the two entry level courses, the first was in Racket (I had never
-      heard of it either). The second was in Java. The more interesting projects
-      came towards the end of the second course. We made a maze generator and solver
-      making use of Kruskals algorithm to generate a minimum spanning tree of grid
-      cells that were connected with random weights. The tree that was generated
-      became the path for the maze, start and end cells were chosen in oposite corners.
-      We built two solvers, one that used BFS, and another that used DFS. The thing
-      was animated and it was just so satisfying to watch it run. Especially after
-      hitting our heads against a few bugs.
+      Fundementals of Computer Science 1 & 2 are the two entry level courses at Northeastern,
+      the first is taught in Racket (I had never heard of it either), the second
+      was in Java. I learned about Functional and OO Programming, the differences,
+      advantages, and disadvantages of each. I'll briefly mention a few of the more
+      interesting projects from Fundies 2.
 
       <br />
-
-      Shortly after this project came the seam carving algorithm. Seam carving
-      is a process of finding the least "important" line of pixels in an image
-      so that you can compress the image (horizonatally or vertically) without
-      warping it (in theory). The concept of "important" was defined by pixel
-      color values in relation to their surrounding pixels. We then used a
-      dynamic programming approach to find the seam with minimum value and
-      removed it from the image.
+      <div class="projectTitle">Maze Solver</div>
       <br />
-      This process was also animated and was also increadibly satisfying to watch.
-      The images got really distorted at the end, but I was surprised with how well
-      the algorithm worked to keep the most interesting pixels in the image.
+
+      The more interesting projects came towards the end of the second course.
+      We made a maze generator and solver making use of Kruskals algorithm to
+      generate a minimum spanning tree of grid cells that were connected with
+      random weights. The tree that was generated became the path for the maze,
+      start and end cells were chosen in oposite corners. We built two solvers,
+      one that used BFS, and another that used DFS. The thing was animated and
+      it was just so satisfying to watch it run. Especially after hitting our
+      heads against a few bugs.
+
+      <br />
+      <div class="projectTitle">Seam Carving</div>
+      <br />
+
+      Seam carving is a process of finding the least "important" line of pixels
+      in an image so that you can remove it to compress the image (horizonatally
+      or vertically) without warping the image (to some extent). The concept of
+      "important" was defined by pixel color values in relation to surrounding
+      pixels. We then used a dynamic programming approach to find the seam with
+      minimum value and removed it from the image.
+      <br />
+      <br />
+      This process was also animated and increadibly satisfying to watch. The images
+      got really distorted at the end, but I was surprised with how well the algorithm
+      worked to keep the most interesting pixels in the image.
+
+      <div class="subtitle">Object Oriented Design</div>
+      Fun course, lots of work, took it over the summer. Fun projects illustrated
+      below:
+
+      <br />
+      <div class="projectTitle">Image Editing GUI</div>
+      <br />
+      This was the last project that we did, it was the culmination of the year of
+      work. We basically created a very primitive image editor that was capable of
+      scaling images down, applying basic filters.
     </div>
   </div>
 </div>
+
+<style>
+  .projectTitle {
+    font-size: 1.6rem;
+    position: relative;
+    display: inline-block;
+    margin-top: 1rem;
+    margin-bottom: 0.5rem;
+  }
+  .projectTitle::after {
+    content: "";
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    width: 100%;
+    height: 2px;
+    background: var(--accent);
+  }
+</style>
