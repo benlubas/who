009ad0ae7316c3a63bac06cbae2de9ee0d5721f9
Ben Lubas <benmlubas@gmail.com>
2021-09-17 19:16:43 -0400

    trying to figure out basepath

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
-  import { Router } from "@sveltech/routify";
+  import { Router, basepath } from "@sveltech/routify";
   import { routes } from "@sveltech/routify/tmp/routes";
+  $basepath = "/public/";
 </script>
 
 <Router {routes} />
