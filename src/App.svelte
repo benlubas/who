e14e6eccd61339ef8a3d566f8f3608e38f26d3a6
benlubas <benmlubas@gmail.com>
2021-09-18 12:14:29 -0400

    changing public to docs for gh-pages

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,16 @@
   import { Router, basepath } from "@sveltech/routify";
   import { routes } from "@sveltech/routify/tmp/routes";
   // $basepath = "/public/";
+  const routifyConfig = {
+    urlTransform: {
+      apply: (url) => url, // external URL
+      remove: (url) => {
+        const partialUrl = `/pages/benlubas/who`;
+        return url.replace(partialUrl, "");
+      }, //internal URL
+    },
+    useHash: true,
+  };
 </script>
 
-<Router {routes} />
+<Router {routes} config={routifyConfig} />
