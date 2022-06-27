3137b38bc5251cbe37b77328b2a9972631e2a5c4
benlubas <benmlubas@gmail.com>
2022-06-27 19:19:55 -0400

    actually adding the work file

---
+++ src/pages/work.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import PageTitle from "../components/PageTitle.svelte";
+</script>
+
+<div class="page-cont">
+  <div class="text">
+    <PageTitle title="Work Experience" />
+    <div class="subtitle">Wayfair (2022/01/10 - 2022/06/24)</div>
+    I worked at Wayfair as a software engineer. This job was a co-op that ran from
+    December 10th to June 24th. I was a part of SEO Pod on the Impression team. The
+    Impressions team at Wayfair solves a lot of different and interesting problems,
+    and they do so using a lot of different technologies. This is perfect for me
+    &mdash; a student that's trying to learn as much as possible. Over the course
+    of the 6 months that I was working at Wayfair, there was always something new
+    for me to learn.
+
+    <br />
+
+    Here is a list of things that I gained experience with at Wayfair:
+
+    <ul>
+      <li>Docker & K8s</li>
+      <li>Spring Boot</li>
+      <li>Kafka with Avro Schemas</li>
+      <li>DataDog</li>
+      <li>Kibana</li>
+      <li>Agile sprint process</li>
+      <li>Jira</li>
+      <li>Buildkite, Sonarcube</li>
+      <li>Little bit of Cypress</li>
+      <li>
+        Working in a massive half spaghetti monolith written in PHP (pain)
+      </li>
+    </ul>
+    <br />
+    <br />
+    Really more than anything, I think I learned what it meant to work on a team
+    at a large corporation. I was able to get a feel for the structure, and the way
+    things operated. At first I felt that a lot of what I was doing was unnecessary,
+    and I still think that some of it was, but over time I started to realize the
+    importance of having an hour long meeting at the end of every sprint to explain
+    the work that we've done to hour business counterparts.
+
+    <br />
+    <br />
+    During my time at Wayfair I got to meet and work with a ton of awesome people,
+    some of whom I'm sure I'll keep in touch with, and others who will most certainly
+    become references on my resume. I'm looking forward to my next co-op, as this
+    experience was really great.
+    <br />
+    <br />
+    This part is for recruiters&mdash;I'm going to talk about my performance at the
+    company. While working for Wayfair I got a midpoint review around the 90 day
+    mark, and a final review right before I left. Both reviews were written by my
+    direct manager who I interacted with alost daily.
+  </div>
+</div>
